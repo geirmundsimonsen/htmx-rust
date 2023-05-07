98c0def0852f737e3f0fb554511ef4bcf21263c4
Geirmund Simonsen <geirmund.simonsen@gmail.com>
2023-05-07 10:22:55 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use std::{net::SocketAddr};
 use axum::{
+  extract::State,
   response::Html,
   routing::get,
   Router,
 };
+use std::net::SocketAddr;
+use std::sync::{Arc, RwLock};
+
+struct Counter {
+  counter: u64,
+}
+
 
 struct Element {
   name: String,
@@ -27,12 +34,12 @@ impl std::fmt::Display for Element {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     let mut attributes = String::new();
     for attribute in &self.attributes {
-        attributes.push_str(&format!("{}=\"{}\" ", attribute.0, attribute.1));
-        //attributes.push(' ');
+      attributes.push_str(&format!("{}=\"{}\" ", attribute.0, attribute.1));
+      //attributes.push(' ');
     }
     let mut children = String::new();
     for child in &self.children {
-        children.push_str(&child.to_string());
+      children.push_str(&child.to_string());
     }
     write!(f, "<{} {}>{}{}</{}>", self.name, attributes, self.content, children, self.name)
   }
@@ -40,8 +47,11 @@ impl std::fmt::Display for Element {
 
 #[tokio::main]
 async fn main() {
+  let shared_state = Arc::new(RwLock::new(Counter { counter: 0 }));
+
   let app = Router::new()
-    .route("/", get(root));
+    .route("/", get(root))
+    .route("/counter", get(counter)).with_state(shared_state);
 
   let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
   axum::Server::bind(&addr)
@@ -55,11 +65,29 @@ async fn root() -> Html<String> {
     elem("head", "", vec![], vec![
       elem("meta", "", vec![ ("charset", "UTF-8") ], vec![]),
       elem("title", "Document", vec![], vec![]),
+      elem("script", "", vec![ ("src", "https://unpkg.com/htmx.org@1.9.2") ], vec![]),
     ]),
     elem(
       "body", "", vec![], vec![
         elem("h1", "Hello, world!", vec![], vec![]),
         elem("p", "Hello, world!", vec![], vec![]),
+        elem("a", "home", vec![ ("href", "/") ], vec![]),
+        elem("a", "counter", vec![ ("href", "/counter") ], vec![]),
+      ],
+    ),
+  ]).to_string())
+}
+
+async fn counter(State(state): State<Arc<RwLock<Counter>>>) -> Html<String> {
+  state.write().unwrap().counter += 1;
+  Html(elem("html", "", vec![ ("lang", "en") ], vec![
+    elem("head", "", vec![], vec![
+      elem("title", "Counter", vec![], vec![]),
+    ]),
+    elem(
+      "body", "", vec![], vec![
+        elem("p", &state.read().unwrap().counter.to_string(), vec![], vec![]),
+        elem("a", "home", vec![ ("href", "/") ], vec![]),
       ],
     ),
   ]).to_string())
