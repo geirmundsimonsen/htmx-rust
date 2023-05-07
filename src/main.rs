785eccadca109f2e48537ad71e75d19f26d0545c
Geirmund Simonsen <geirmund.simonsen@gmail.com>
2023-05-07 09:50:52 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,66 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr};
 use axum::{
-    response::Html,
-    routing::get,
-    Router,
+  response::Html,
+  routing::get,
+  Router,
 };
 
 struct Element {
-    name: String,
-    content: String,
-    attributes: Vec<String>,
-    children: Vec<Element>,
+  name: String,
+  content: String,
+  attributes: Vec<(String, String)>,
+  children: Vec<Element>,
 }
 
-fn elem(name: String, content: String, attributes: Vec<String>, children: Vec<Element>) -> Element {
-    Element {
-        name,
-        content,
-        attributes,
-        children,
-    }
+fn elem(name: &str, content: &str, attributes: Vec<(&str, &str)>, children: Vec<Element>) -> Element {
+  Element {
+    name: name.to_string(),
+    content: content.to_string(),
+    attributes: attributes.iter().map(|attr| {
+      (attr.0.to_string(), attr.1.to_string())
+    }).collect(),
+    children,
+  }
 }
 
 impl std::fmt::Display for Element {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut attributes = String::new();
-        for attribute in &self.attributes {
-            attributes.push_str(&attribute);
-            attributes.push(' ');
-        }
-        let mut children = String::new();
-        for child in &self.children {
-            children.push_str(&child.to_string());
-        }
-        write!(f, "<{} {}>{}{}</{}>", self.name, attributes, self.content, children, self.name)
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    let mut attributes = String::new();
+    for attribute in &self.attributes {
+        attributes.push_str(&format!("{}=\"{}\" ", attribute.0, attribute.1));
+        //attributes.push(' ');
     }
+    let mut children = String::new();
+    for child in &self.children {
+        children.push_str(&child.to_string());
+    }
+    write!(f, "<{} {}>{}{}</{}>", self.name, attributes, self.content, children, self.name)
+  }
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(root));
+  let app = Router::new()
+    .route("/", get(root));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+  axum::Server::bind(&addr)
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
 }
 
 async fn root() -> Html<String> {
-    Html(elem(
-        "html".to_string(),
-        "".to_string(),
-        vec![
-            "lang=\"en\"".to_string(),
-        ],
-        vec![
-            elem(
-                "head".to_string(),
-                "".to_string(),
-                vec![],
-                vec![
-                    elem(
-                        "meta".to_string(),
-                        "".to_string(),
-                        vec![
-                            "charset=\"UTF-8\"".to_string(),
-                        ],
-                        vec![],
-                    ),
-                    elem(
-                        "meta".to_string(),
-                        "".to_string(),
-                        vec![
-                            "name=\"viewport\"".to_string(),
-                            "content=\"width=device-width, initial-scale=1.0\"".to_string(),
-                        ],
-                        vec![],
-                    ),
-                    elem(
-                        "title".to_string(),
-                        "Document".to_string(),
-                        vec![],
-                        vec![],
-                    ),
-                ],
-            ),
-            elem(
-                "body".to_string(),
-                "".to_string(),
-                vec![],
-                vec![
-                    elem(
-                        "h1".to_string(),
-                        "Hello, world!".to_string(),
-                        vec![],
-                        vec![],
-                    ),
-                ],
-            ),
-        ],
-    ).to_string())
+  Html(elem("html", "", vec![ ("lang", "en") ], vec![
+    elem("head", "", vec![], vec![
+      elem("meta", "", vec![ ("charset", "UTF-8") ], vec![]),
+      elem("title", "Document", vec![], vec![]),
+    ]),
+    elem(
+      "body", "", vec![], vec![
+        elem("h1", "Hello, world!", vec![], vec![]),
+        elem("p", "Hello, world!", vec![], vec![]),
+      ],
+    ),
+  ]).to_string())
 }
